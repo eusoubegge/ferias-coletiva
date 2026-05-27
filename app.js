9b55634cc360a391d774ab0d0340b08979348ad9
eusoubegge <mleandro.pollas@gmail.com>
2026-05-27 10:54:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+ferias-coletiva/
+├── package.json          ← arquivo do artefato "package.json"
+├── public/
+│   └── index.html        ← arquivo do artefato "public/index.html"
+└── src/
+    ├── index.js          ← arquivo do artefato "src/index.js"
+    └── App.js            ← código copiado do artefato principal
